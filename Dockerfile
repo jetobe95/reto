50a1731a8b732390278982b3454bdf09a7a2c71a
Jeffrey <alto.951@hotmail.com>
2019-07-10 22:54:51 -0500

    Preparacion para docker

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM node:8.2.1-alpine
+
+WORKDIR /usr/src/app
+
+COPY package*.json ./
+
+RUN npm install
+
+COPY . .
+
+EXPOSE 4000
+
+CMD ["npm","start"]
--- mongodb.connection.js
+++ mongodb.connection.js
@@ -1,3 +1,3 @@
 const mongose = require('mongoose')
-const url = 'mongodb://localhost/login-bloqueo'
+const url = 'mongodb://mongo:27017/login-bloqueo'
 module.exports = mongose.connect(url, { useNewUrlParser: true })
